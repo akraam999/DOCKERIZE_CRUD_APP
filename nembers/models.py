5fc68cdd875c36ee10501f6e6e6494860bc7275b
Med Akram <mohammed-akram-moufid@ensam-casa.ma>
2023-03-10 14:25:12 +0100

    akr

--- nembers/models.py
+++ nembers/models.py
@@ -20,6 +20,5 @@ class Project(models.Model):
 class Admin(User):
     username = models.CharField(max_length=100)
     email = models.CharField(max_length=250)
-    password = models.CharField(max_length=250)
 
     
